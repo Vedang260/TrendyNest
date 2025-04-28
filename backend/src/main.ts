3ec94a56710aaefb7e6de4a5bec3d9ee34762d69
Vedang <vedang2607@gmail.com>
2025-04-28 15:39:42 +0530

    added the validation pipes & global prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import { join } from 'path';
 
+dotenv.config()
+//console.log('JWT_SECRET:', process.env.JWT_SECRET);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+  app.enableCors();
+  
+  // serving static files from uploads directory
+  //app.use('/videos', express.static(join(__dirname, '..', 'uploads/videos')));
+  await app.listen(process.env.PORT ?? 8000);
+  console.log('🚀Server is running on port: 8000')
 }
 bootstrap();
