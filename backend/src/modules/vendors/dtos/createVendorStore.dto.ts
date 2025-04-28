4c1f3a56c461fc689a09c1c642e45b0449172147
Vedang <vedang2607@gmail.com>
2025-04-28 19:26:45 +0530

    create Vendor Store dto is created

---
+++ backend/src/modules/vendors/dtos/createVendorStore.dto.ts
@@ -0,0 +1,32 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateVendorDto{
+    @IsNotEmpty()
+    @IsString()
+    vendorId: string;
+
+    @IsNotEmpty()
+    @IsString()
+    categoryId: string;
+
+    @IsNotEmpty()
+    @IsString()
+    addressId: string;
+
+    @IsNotEmpty()
+    @IsString()
+    store_name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    store_description: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsEmail()
+    business_email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    business_phone: string;
+}
