ac2b5a99a3f9cdab1adbb8abef09df6d2c0be3f9
Vedang <vedang2607@gmail.com>
2025-04-28 17:42:09 +0530

    added category module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,13 +5,15 @@ import { AuthModule } from './modules/auth/module/auth.module';
 import { typeOrmConfig } from './config/database.config';
 import { AuthMiddleware } from './modules/auth/middlewares/auth.middleware';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CategoryModule } from './modules/categories/module/category.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(typeOrmConfig),
     AuthModule,
-    UsersModule
+    UsersModule,
+    CategoryModule
   ],
 })
 export class AppModule {
@@ -20,6 +22,7 @@ export class AppModule {
     .apply(AuthMiddleware)
     .forRoutes(
       { path: 'users', method: RequestMethod.ALL },
+      { path: 'categories', method: RequestMethod.ALL }
     );
   }
 }
