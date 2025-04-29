4c919516747bb802fc9cac529ca8a249cd872e62
Vedang <vedang2607@gmail.com>
2025-04-29 15:55:34 +0530

    cart-items controller is created

---
+++ backend/src/modules/cart/controllers/cartItems.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Param, Delete, UseGuards, Post, Put, Body, Req } from '@nestjs/common';
+import { JwtAuthGuard } from '../../auth/guards/jwt_auth.guard';
+import { RolesGuard } from '../../auth/guards/roles.guard';
+import { Roles } from '../../../common/decorators/roles.decorator';
+import { UserRole } from '../../../common/enums/roles.enums';
+import { CartItemsService } from '../services/cartItems.service';
+import { CreateCartItemDto } from '../dtos/createCartItem.dto';
+
+@Controller('cart-items')
+@UseGuards(JwtAuthGuard)
+export class CartItemsController {
+  constructor(private readonly cartItemsService: CartItemsService) {}
+
+  @Get(':id')
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  async findAll(@Param('id') id: string){
+    return this.cartItemsService.getAllCartItems(id);
+  }
+
+  @Delete(':id')
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  async removeCartItem(@Param('id') id: string){
+    return this.cartItemsService.removeCartItem(id);
+  }
+
+  @Post('add')
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  async addCartItem(@Body('cartItem') createCartItemDto: CreateCartItemDto, @Req() req: Request){
+    return this.cartItemsService.addCartItem(req['user'].userId, createCartItemDto);
+  }
+
+  @Put(':id')
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.CUSTOMER)
+  async updateCartItem(@Param('id') id: string, @Body('quantity') quantity: number){
+    return this.cartItemsService.updateCartItem(id, quantity);
+  }
+} 
