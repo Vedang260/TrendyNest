fd482dc82330e8774cb461515d2a1566196fe9cd
Vedang <vedang2607@gmail.com>
2025-05-01 14:02:40 +0530

    added api

---
+++ frontend/src/services/vendorStores/api.ts
@@ -0,0 +1,87 @@
+const API_BASE_URL = 'http://localhost:8000/api';
+import axios from 'axios';
+
+export const fetchAdminApprovedVendorStores = async (token: string) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/products/admin/approved`, {
+        headers: {
+            'Authorization': `Bearer ${token}`, 
+            'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error: any) {
+      throw new Error(error.response?.data?.message || 'Error in Fetching the approved products');
+    }
+};
+
+export const fetchAdminPendingVendorStores = async (token: string) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/products/admin/pending`, {
+        headers: {
+            'Authorization': `Bearer ${token}`, 
+            'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error: any) {
+      throw new Error(error.response?.data?.message || 'Error in Fetching the pending products');
+    }
+};
+
+export const fetchAdminRejectedVendorStores = async (token: string) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/products/admin/rejected`, {
+        headers: {
+            'Authorization': `Bearer ${token}`, 
+            'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error: any) {
+      throw new Error(error.response?.data?.message || 'Error in Fetching the rejected products');
+    }
+};
+
+export const approveProduct = async(productId: string, token: string) => {
+    try {
+        const status = 'approved';
+        
+        const response = await axios.put(`${API_BASE_URL}/products/${productId}`, {
+            product: {
+                status: status
+            }
+        }, {
+          headers: {
+              'Authorization': `Bearer ${token}`, 
+              'Content-Type': 'application/json',
+          },
+        });
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || 'Error in approving the product');
+    }
+};
+
+export const rejectProduct = async(productId: string, token: string) => {
+    try {
+        const status = 'rejected';
+        
+        const response = await axios.put(`${API_BASE_URL}/products/${productId}`, {
+            product: {
+                status: status
+            }
+        }, {
+          headers: {
+              'Authorization': `Bearer ${token}`, 
+              'Content-Type': 'application/json',
+          },
+        });
+        return response.data;
+    } catch (error: any) {
+    throw new Error(error.response?.data?.message || 'Error in rejecting the product');
+    }
+};
