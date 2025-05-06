a4db3aef61e30891f4f7d66120b9262f3c73e125
Vedang <vedang2607@gmail.com>
2025-05-06 13:11:21 +0530

    added module imports

--- backend/src/utils/pricing/pricing.module.ts
+++ backend/src/utils/pricing/pricing.module.ts
@@ -4,11 +4,15 @@ import { PricingService } from './pricing.service';
 import { PricingProcessor } from './pricing.processor';
 import { ProductService } from '../../modules/products/services/products.service'; // adjust path
 import { ScheduleModule } from '@nestjs/schedule';
+import { OrderItemsModule } from 'src/modules/orders/modules/orderItems.module';
+import { ProductsModule } from 'src/modules/products/module/products.module';
 
 @Module({
   imports: [
     BullModule.registerQueue({ name: 'pricing' }),
     ScheduleModule.forRoot(),
+    OrderItemsModule,
+    ProductsModule
   ],
   providers: [PricingService, PricingProcessor, ProductService],
 })
--- backend/src/utils/pricing/pricing.service.ts
+++ backend/src/utils/pricing/pricing.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
 import { Cron } from '@nestjs/schedule';
+import { ProductService } from '../product/product.service';
 
 @Injectable()
 export class PricingService{
